aacf13bbbbdb6f187e36a7891427d7b45c7f4373
Ezequiel Fernandez <ezzefernandezzz@gmail.com>
2022-10-19 00:05:52 -0300

    Se elimina codigo en deshuso

--- Entregable3/src/main/java/ArquitecturaWeb/Entregable3/controller/EstudianteController.java
+++ Entregable3/src/main/java/ArquitecturaWeb/Entregable3/controller/EstudianteController.java
@@ -7,7 +7,6 @@ import ArquitecturaWeb.Entregable3.service.EstudianteService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +23,6 @@ public class EstudianteController {
     public List<Estudiante> getAllPersonas() {
         return estudianteService.findAll();
     }
-    
-   /*  @RequestMapping(value = "/estudiantes/{dni}", method = RequestMethod.GET, produces = "application/json")
-    public Estudiante getPersonaByDni(@PathVariable int dni) {
-        return estudianteService.findByDni(dni);
-    } */
-
 
     @RequestMapping(value = "estudiantes/{lu}", method = RequestMethod.GET, produces = "application/json")
     public Estudiante getEstudianteByLU(@PathVariable int lu){
@@ -50,11 +43,6 @@ public class EstudianteController {
     public Estudiante addStudent(@RequestBody Estudiante e){
         return estudianteService.save(e);
     }
-
-    /* @RequestMapping(value = "estudiantes", params={"carrera","nombre"}, method = RequestMethod.GET, produces = "application/json")
-    public List<Carrera> getEstudiantesbyCarrera(@RequestParam int carrera, @RequestParam String nombre){
-        return estudianteService.findByCarrera(carrera, nombre);
-    } */
 
     @RequestMapping(value = "estudiantes", params={"carrera","ciudad"}, method = RequestMethod.GET, produces = "application/json")
     public List<Estudiante> getEstudiantesbyCarreraYCiudad(@RequestParam int carrera, @RequestParam String ciudad){
